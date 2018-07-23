8862d38ac37daa87b1d6803f61e860d122b222d2
alibain10 <alibain10@gmail.com>
2018-07-23 14:38:16 -0700

    fixed code

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Justin',
+		photo: 'https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/justinbieber-hero-498329920.jpg?itok=03MYx1T4',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Miley',
+		photo: 'https://cdna.hairromance.com/wp-content/uploads/2012/08/Miley-Cyrus-new-platinum-short-hair-style.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+    },
+    {
+		name: 'Ariana',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDb32B7U0xRZ015mtqeVvN8DOakyEmgo4CEnE-5U2YlXl4eJ4Mvg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+];
+
+module.exports = friendsArray;
