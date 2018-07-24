9fda787aad2780dc6a7b2c1feb09eabf3d24ddf4
alibain10 <alibain10@gmail.com>
2018-07-24 07:10:12 -0700

    fixed js code

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,52 +11,46 @@ var friendsData = require("../data/friends.js");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+    // API POST Requests
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
+    app.post('/api/friends', function (req, res) {
+        // to compare the scores
+        var newFriendScoreArray = req.body.scores;
+        var scoresArray = [];
+        var bestMatch = 0;
+        var count = 0;
+        // for current friend 
+        for (var i = 0; i < friendsData.length; i++) {
+            var scoresDiff = 0;
+            // compare the friend with existing friend
+            for (var j = 0; j < newFriendScoreArray.length; j++) {
+                scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScoreArray[j])));
+            }
+            // then add the json the user sent to the friendsData array 
+            scoresArray.push(scoresDiff);
+        }
+        // find best match after comparision  with all friends
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+        //return data
+        var yourMatch = friendsData[bestMatch];
+        res.json(yourMatch);
+        friendsData.push(req.body);
+    });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });
 };
